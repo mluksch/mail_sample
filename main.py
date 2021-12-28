6c30143ade0343c38cdadc4652ee4e19d804ff5b
mluk <mikauschkal@gmail.com>
2021-12-28 23:51:45 +0100

    switched to mailgun

--- main.py
+++ main.py
@@ -7,14 +7,13 @@ from dotenv import load_dotenv
 load_dotenv()
 MAIL_GUN_API_KEY = os.getenv("MAIL_GUN_API_KEY")
 MAILGUN_ENDPOINT = os.getenv("MAILGUN_ENDPOINT")
-MAIL_GUN_SENDER_DOMAIN = os.getenv("SENDER_DOMAIN")
 RECEIVER = os.getenv("RECEIVER")
 SENDER = os.getenv("SENDER")
 
 
 def send_simple_message(subject, text):
     return requests.post(
-        f"https://api.mailgun.net/v3/{MAIL_GUN_SENDER_DOMAIN}/messages",
+        f"{MAILGUN_ENDPOINT}/messages",
         auth=("api", MAIL_GUN_API_KEY),
         data={"from": SENDER,
               "to": [RECEIVER],
